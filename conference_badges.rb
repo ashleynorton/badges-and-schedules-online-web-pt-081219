4e93f875171c818866cc06b5e465584383864ee7
Ashley Norton <nortonashleym@gmail.com>
2019-08-22 16:05:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,12 +1,12 @@
 def badge_maker(name)
-  puts "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
   attendees.map do |name|
     badge_maker(name)
   end
-end 
+end
 
 def assign_room(name, number)
   "Hello, #{name}! You'll be assigned to room #{number}!"
@@ -20,6 +20,13 @@ def assign_rooms(attendees)
     counter += 1
   end
   result
+end
+
+def printer(attendees)
+  attendees.each_with_index do |name, index|
+    puts badge_maker(name)
+    puts assign_room(name, index + 1)
+  end
 end
 
 def printer(attendees)
