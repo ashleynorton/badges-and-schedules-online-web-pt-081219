23be58c51eae2919d75c420954ec062d0df821d4
Ashley Norton <nortonashleym@gmail.com>
2019-08-22 15:40:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+#conference_speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker (name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (array)
+  new_array = []
+  array.each do |speaker|
+    new_array << badge_maker(speaker)
+  end
+end 
+
+def assign_rooms
+  
+end 
+
+def printer
+  
+end 
+
+
+
